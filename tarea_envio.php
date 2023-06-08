16b405534c53637ef0379c37bec4b92053725f00
JorgeGuerra91 <136023100+JorgeGuerra91@users.noreply.github.com>
2023-06-08 17:45:13 -0500

    Update tarea_envio.php

--- tarea_envio.php
+++ tarea_envio.php
@@ -3,25 +3,11 @@ session_start();
 include 'conexion.php';
 
 
-function revisaremp($documento,$tipo_doc,$con){
-    $sql="SELECT * from usuarios where doc_usu='$documento' and tipo_id_usu='$tipo_doc'";
-    $result=mysqli_query($con,$sql);
-    if(mysqli_num_rows($result) > 0){
-        return 1;
-    }else{
-        return 0;
-    }
-}
-
 
 if (isset($_POST['guardar1'])) {
     {    
-		if(strlen($_POST['tarea'])>=1){
-			$auto = trim($_POST['tarea']);
-		}
-		
+	
 		$tarea = trim($_POST['tarea']);
-		$fecha_solicitud = $date;
 	
 		$update = "INSERT INTO `tareas`(`tarea`) VALUES ('$tarea')";
 		$result = mysqli_query($con, $update);
